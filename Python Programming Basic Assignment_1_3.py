033b3f7a82b5725788352448cb272d51de1d9bfa
Harshil Chauhan <91827913+harshil13031995@users.noreply.github.com>
2022-05-21 22:23:18 +0530

    Delete Python Programming Basic Assignment_1_3.py

--- Python Programming Basic Assignment_1_3.py
+++
@@ -1,9 +0,0 @@
-def area():
-    """to find area of triangle"""
-    base=float(input('Enter value of base : '))
-    height=float(input('Enter value of height : '))
-    area=0.5*base*height
-    print(area)
-    return area
-
-area()
